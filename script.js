004a04788cbda81e3f0abae3fae8d2d385fbaf08
vudjaaz <vudjaaz@mail.ru>
2024-05-24 11:48:10 +0300

    ДЗ 1

---
+++ script.js
@@ -0,0 +1,66 @@
+// console.log("Текст из script.js");
+
+// const c = 10;
+// const d = 2;
+
+// const fold = c + d;
+
+// const diff = c - d;
+
+// const multi = c * d;
+
+// const division = c / d;
+
+// console.log(fold, diff, multi, division);
+
+// const sum = fold + diff + multi + division;
+// console.log(sum);
+
+// const a = 1.5;
+// const b = 0.75;
+// console.log(a + b);
+
+// let c = 123;
+// console.log(-c);
+
+// let firstName = 'Yurii';
+// let lastName = 'Borskii';
+
+// console.log(firstName, lastName);
+
+// let temp = 'java';
+// let temp2 = 'script';
+
+// console.log(temp + temp2);
+
+// let temp3 = 'Hello';
+// let temp4 = 'world';
+
+// console.log(temp3 + ' ' + temp4);
+
+// temp = 'java';
+// temp2 = 'script';
+
+// console.log(`${temp + temp2}`);
+
+// temp3 = 'Hello';
+// temp4 = 'world';
+
+// console.log(`${temp3} ${temp4}`);
+
+
+
+console.log("ДЗ семинар 1-2 задание 1");
+
+const celsius = 28;
+const fahrenheit = (9 / 5) * celsius + 32;
+
+console.log(`Температура в Цельсиях ${celsius}, в Фаренгейтах ${fahrenheit}`);
+
+
+console.log("ДЗ семинар 1-2 задание 2");
+
+const firstName = 'Yurii';
+let admin = firstName;
+
+console.log(admin);
